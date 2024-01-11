91d912bbdd37693fc88a23f6b13bf22beecf85c3
user <user@DESKTOP-KIHC0DT>
2024-01-11 14:56:07 +0100

    invio file

---
+++ src/grandprix_gruppo3_5ainf/GrandPrix_Gruppo3_5AINF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grandprix_gruppo3_5ainf;
+
+/**
+ *
+ * @author user
+ */
+public class GrandPrix_Gruppo3_5AINF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
