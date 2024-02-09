99718bc71b41d93fa1b5d19438d93be4fe156042
user <user@DESKTOP-KIHC0DT>
2024-02-09 14:55:59 +0100

    implementazione classe Giocatore

---
+++ src/grandprix_gruppo3_5ainf/Giocatore.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grandprix_gruppo3_5ainf;
+
+/**
+ *
+ * @author user
+ */
+public class Giocatore {
+    String nome;
+    String cognome;
+    String pwd;
+    
+    /**
+     * @param nome
+     * @param cognome
+     * @param pwd
+     */
+    
+    public Giocatore(String nome, String cognome, String pwd){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.pwd = pwd;
+    }
+       
+}
